61cfa8291af444ae8db43c0b68b271eb05039e65
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2024-11-05 09:53:00 +0900

    파일 이동 완료(타입 수정필요)

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,74 @@
-import React from 'react'
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Icon from "@mui/material/Icon";
 
-export default function Dashboard() {
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+
+// Soft UI Dashboard React examples
+import DashboardLayout from "components/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "components/Navbars/DashboardNavbar";
+import Footer from "components/Footer";
+
+// Soft UI Dashboard React base styles
+import typography from "assets/theme/base/typography";
+
+
+function Dashboard() {
+  const { size } = typography;
+
   return (
-    <div>dashboard</div>
-  )
+    <DashboardLayout>
+      <DashboardNavbar />
+      <SoftBox py={3}>
+        <SoftBox mb={3}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} xl={3}>
+              
+            </Grid>
+            <Grid item xs={12} sm={6} xl={3}>
+              
+            </Grid>
+            <Grid item xs={12} sm={6} xl={3}>
+              
+            </Grid>
+            <Grid item xs={12} sm={6} xl={3}>
+              
+            </Grid>
+          </Grid>
+        </SoftBox>
+        <SoftBox mb={3}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} lg={7}>
+              
+            </Grid>
+            <Grid item xs={12} lg={5}>
+              
+            </Grid>
+          </Grid>
+        </SoftBox>
+        <SoftBox mb={3}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} lg={5}>
+              
+            </Grid>
+            <Grid item xs={12} lg={7}>
+              
+            </Grid>
+          </Grid>
+        </SoftBox>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6} lg={8}>
+            
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            
+          </Grid>
+        </Grid>
+      </SoftBox>
+      <Footer />
+    </DashboardLayout>
+  );
 }
+
+export default Dashboard;
